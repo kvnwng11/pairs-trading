d24dc1aaf12276ca15ef5c65fb6ff628e4b35a0b
kvnwng <kvnwng@umich.edu>
2022-07-05 13:42:22 -0400

    Switch from Coinbase API to Binance

--- stream_prices.py
+++ stream_prices.py
@@ -1,62 +1,52 @@
 import numpy as np
 import pandas as pd
-import datetime
+import datetime as dt
 import os
 from collections import deque
 from io import StringIO
-import cbpro  # coinbase pro api
+from binance.client import Client
 
-client = cbpro.PublicClient()
+api_key = 'XMkgHm97q4ScccpiRb1Lf0Ta8BYanEPHBjIgwZatDqaNkPcgfnuSUFaWLdOL97IF'
+api_secret = '5E2PnhoRqx0nPD3QZ7cJaGvKgnlHuiGpd15x94eXKKaWvk2DtO8qNylrC63otSIj'
+client = Client(api_key, api_secret)
+
 path = 'csv/'
-symbols = ['DOGE', 'SHIB', 'BTC', 'LTC', 'MATIC']
+symbols = ['DOGE', 'SHIB', 'BTC', 'LTC', 'MATIC', 'XRP']
 
 
 def get_historical_prices():
-    date = pd.Timestamp.now().round(freq='s')
+    def GetHistoricalData(coin, start_time, end_time):
+        # Calculate the timestamps for the binance api function
+        untilThisDate = end_time
+        sinceThisDate = start_time
+        # Execute the query from binance - timestamps must be converted to strings !
+        candle = client.get_historical_klines(f'{coin}USDT', Client.KLINE_INTERVAL_1MINUTE, str(sinceThisDate), str(untilThisDate))
+
+        # Create a dataframe to label all the columns returned by binance so we work with them later.
+        df = pd.DataFrame(candle, columns=['time', 'open', 'high', 'low', 'close', 'volume', 'closeTime',
+                          'quoteAssetVolume', 'numberOfTrades', 'takerBuyBaseVol', 'takerBuyQuoteVol', 'ignore'])
+        # as timestamp is returned in ms, let us convert this back to proper timestamps.
+        df['time'] = pd.to_datetime(df['time'], unit='ms').dt.strftime("%Y-%m-%d %H:%M:%S")
+        df.set_index('time', inplace=True)
+
+        # Get rid of columns we do not need
+        df = df.drop(['closeTime', 'quoteAssetVolume', 'numberOfTrades',
+                     'takerBuyBaseVol', 'takerBuyQuoteVol', 'ignore'], axis=1)
+
+        return df['close']
 
     # create and populate needed csvs
     for asset in symbols:
         asset_path = path+asset+'.csv'
         # if no csv file exists, pull minute data from the last 30 days
         if not os.path.exists(asset_path):
-            
-            end = (pd.Timestamp.now()).round(
-                freq='T') - pd.Timedelta(minutes=1)
-            start = end - pd.Timedelta(days=30)
-            until = start + pd.Timedelta(minutes=300)
-            
-            print(asset)
-            prices = pd.DataFrame()
-            """
-            end = datetime.datetime.utcnow()
-            end = end-datetime.timedelta(minutes=1, seconds=end.second, microseconds=end.microsecond)
-            start = end - datetime.timedelta(days=30)
-            until = end + datetime.timedelta(minutes=300)
-            """
-            unsupported = 0
-            while until <= end:
-                data = np.array(client.get_product_historic_rates(asset+'-USD', granularity=60, start=start, end=until))
-                if data.size == 1:
-                    print("data size is 1")
-                    print(data)
-                    print("start: ", start)
-                    print("until: ", until)
-                    break
-                elif len(data) == 0:
-                    print("unsupported token")
-                    unsupported = 1
-                    break
-                data = data[::-1]
-                df = pd.DataFrame(data)
-                prices = pd.concat([prices, df], ignore_index=True, axis=0)
-                start += pd.Timedelta(minutes=300) #datetime.timedelta(minutes=300)
-                until += pd.Timedelta(minutes=300) #datetime.timedelta(minutes=300)
-            if unsupported == 1:
-                continue
-            prices.columns = ['time', 'low', 'high', 'open', 'close', 'volume']
-            prices['time'] = pd.to_datetime(prices['time'], unit='s')
+            end = dt.datetime.utcnow()
+            end = end - dt.timedelta(minutes=1, seconds=end.second, microseconds=end.microsecond)
+            start = end - dt.timedelta(days=30)
+
+            prices = pd.DataFrame(GetHistoricalData(asset, start, end))
             prices.drop_duplicates(subset='time', keep='last', inplace=True)
-            prices[['time', 'close']].to_csv(asset_path, mode='a', header=False, index=False)
+            prices[['time', 'close']].to_csv(asset_path, mode='w', header=False, index=False)
 
 def update_prices():
     # update any gaps in the csv file
@@ -68,26 +58,31 @@ def update_prices():
             q = deque(f, N)
         prices = pd.read_csv(StringIO(''.join(q)), header=None)
         prices.columns = ['time', 'close']
-        start = pd.to_datetime(prices['time'].iloc[-1]) + pd.Timedelta(minutes=1)
+        start = pd.to_datetime(
+            prices['time'].iloc[-1]) + pd.Timedelta(minutes=1)
         end = (pd.Timestamp.now() - pd.Timedelta(minutes=1)).round(freq='T')
         until = start + min(pd.Timedelta(minutes=300), end-start)
 
         if start <= end:
             while until <= end:
-                data = np.array(client.get_product_historic_rates(asset+'-USD', granularity=60, start=start, end=until))
+                data = np.array(client.get_product_historic_rates(
+                    asset+'-USD', granularity=60, start=start, end=until))
                 if data.size == 1:
                     break
                 data = data[::-1]
                 df = pd.DataFrame(data)
-                
+
                 df.columns = ['time', 'low', 'high', 'open', 'close', 'volume']
-                df.drop(['low', 'high', 'open', 'volume'],axis=1, inplace=True)
+                df.drop(['low', 'high', 'open', 'volume'],
+                        axis=1, inplace=True)
                 df['time'] = pd.to_datetime(df['time'], unit='s')
                 prices = pd.concat([prices, df], ignore_index=True, axis=0)
                 start += pd.Timedelta(minutes=300)
                 until += pd.Timedelta(minutes=300)
             prices.drop_duplicates(subset='time', keep='last', inplace=True)
-            prices[['time', 'close']].to_csv(asset_path, mode='a', header=False, index=False)
+            prices[['time', 'close']].to_csv(
+                asset_path, mode='a', header=False, index=False)
+
 
 get_historical_prices()
-update_prices()
+#update_prices()
