4d4e8db1aca2817f69bd3521d2eea36416d9e684
kvnwng <kvnwng@umich.edu>
2022-07-13 11:18:22 -0400

    Add derivative logic to trade.py

--- trade.py
+++ trade.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import statsmodels.api as sm
 from collections import deque
 from io import StringIO
 
@@ -9,6 +9,8 @@ state_path = '/home/kvnwng11/code/pairs-trading/state/'
 statefile = ''
 
 pair = ['', '']
+x_label = pair[0]
+y_label = pair[1]
 
 window = 30 * 1440
 stop_loss = -0.05
@@ -56,56 +58,63 @@ old_signal = signal
 old_position0 = position0
 old_position1 = position1
 past_data = raw_data[[pair[0], pair[1]]][t-window-1:t-1]
-past_price0 = np.asarray(past_data[pair[0]])
-past_price1 = np.asarray(past_data[pair[1]])
-curr_price0 = raw_data[pair[0]][t]
-curr_price1 = raw_data[pair[1]][t]
-past_spread = np.log(past_price0) - np.log(past_price1)
-curr_spread = np.log(curr_price0) - np.log(curr_price1)
+x = np.array(past_data[x_label])
+y = np.array(past_data[y_label])
+curr_x = raw_data[x_label][t]
+curr_y = raw_data[y_label][t]
+
+# simple beta
+reg = sm.OLS(np.log(y), sm.add_constant(np.log(x)))
+reg = reg.fit()
+a0 = reg.params[0]
+b0 = reg.params[1]
+hedge_ratio = b0
+
+# find current zscore
+past_spread = np.log(y) - hedge_ratio*np.log(x)
+curr_spread = np.log(curr_y) - hedge_ratio*np.log(curr_x)
 curr_zscore = zscore(past_spread, curr_spread)
 
-"""
-TODO: implement hedge ratio and fix return logic bug
-
-# Johansen test for hedge ratio
-res = coint_johansen(raw_data[[symbols[0],symbols[1]]], det_order=0, k_ar_diff=1)
-print("Eigenvalue critical values:\n", res.cvm)
-print("Eigenvalue statistic:\n", res.max_eig_stat)
-print("Eigenvector:\n", res.evec)
-johansen_stat = res.max_eig_stat[1]
-b0 = res.evec[0][0]
-b1 = res.evec[1][0]
-"""
-hedge_ratio = 1 #abs(b1/b0)
+# find derivatives
+first_d = np.gradient((past_spread))
+second_d = np.gradient(first_d)
+first_d = first_d[-1]
+second_d = second_d[-1]
 
 # check for stop loss
 if current_return < stop_loss:
     signal = 0
 # check if still in trade
 else:
-    # exit signal
+    # decide to exit
     if curr_zscore >= exit_zscore-0.1 and curr_zscore <= exit_zscore+0.1:
-        signal = 0
-    # still in position
-    elif np.sign(curr_zscore) == old_signal:
+        signal = old_signal = 0
+    elif signal == 1 and first_d > 0 and second_d > 0:
+        signal = old_signal = 0
+    elif signal == -1 and first_d < 0 and second_d < 0:
+        signal = old_signal = 0
+    
+    # decide to trade
+    if np.sign(curr_zscore) == old_signal:
         signal = old_signal
         position0 = old_position0
         position1 = old_position1
-    # trade signal if above 1 std
-    elif curr_zscore > entry_zscore:
+    # sell signal
+    elif curr_zscore > entry_zscore and first_d < 0 and second_d < 0:
         signal = 1
-        position0 = signal
-        position1 = -hedge_ratio*signal
-    elif curr_zscore < -entry_zscore:
+        position1 = signal
+        position0 = -hedge_ratio*signal
+    # buy signal
+    elif curr_zscore < -entry_zscore and first_d > 0 and second_d > 0:
         signal = -1
-        position0 = signal
-        position1 = -hedge_ratio*signal
+        position1 = signal
+        position0 = -hedge_ratio*signal
+    # default: do nothing
     else:
-        signal = old_signal
+        signal = 0
 
 # calculate returns
-gross = position0*(curr_price0/raw_data[pair[0]][t-1] - 1) + \
-    position1*(curr_price1/raw_data[pair[1]][t-1] - 1)
+gross = position0*(curr_x/raw_data[x_label][t-1] - 1) + position1*(curr_y/raw_data[y_label][t-1] - 1)
 net = gross - commission * \
     (abs(position0 - old_position0) + abs(position1 - old_position1))
 if signal == old_signal:
@@ -113,6 +122,9 @@ if signal == old_signal:
 else:
     current_return = gross
     numtrades += 1
+
+if signal == 0:
+    current_return = 0
 
 # calculate returns
 balance *= (1+net)
